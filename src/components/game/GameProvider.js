5f792b7143a9f8625e0bf6450ebf119bfdaf005e
hann <hmweeks93@gmail.com>
2021-08-10 10:24:23 -0500

    created fetch calls for gametypes in provider

--- src/components/game/GameProvider.js
+++ src/components/game/GameProvider.js
@@ -4,6 +4,8 @@ export const GameContext = React.createContext()
 
 export const GameProvider = (props) => {
     const [ games, setGames ] = useState([])
+    const [ gameTypes, setGameTypes ] = useState([])
+
 
     const getGames = () => {
         return fetch("http://localhost:8000/games", {
@@ -15,8 +17,33 @@ export const GameProvider = (props) => {
             .then(setGames)
     }
 
+    const createGame = (game) => {
+        return fetch("http://localhost:8000/games", {
+            method: "POST",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`,
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(game)
+         })
+            .then(getGames)
+    }
+    
+    const getGameTypes = () => {
+        return fetch("http://localhost:8000/gametypes", { 
+            method: "GET",
+            headers: {
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+          }  
+        })
+            .then(res => res.json())
+            .then(setGameTypes)
+    }
+    
     return (
-        <GameContext.Provider value={{ games, getGames }} >
+        <GameContext.Provider value={{ games, getGames, getGameTypes, createGame, gameTypes
+        
+        }} >
             { props.children }
         </GameContext.Provider>
     )
